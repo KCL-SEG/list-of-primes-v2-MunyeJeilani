d02d76f660967da4cf89708e1b0ba2634d0df0b9
Munye Jeilani <munye.jeilani@kcl.ac.uk>
2023-12-04 15:06:03 +0000

    Passes 5/5  tests.

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("The value must be a positive integer , increase your number")
+
     list = []
+    current_value_being_checked = 2  # Start with the first prime number
+
+    while len(list) < number_of_primes:
+        number_is_prime = True
+        for factor in range(2, int(current_value_being_checked**0.5) + 1):
+            if current_value_being_checked % factor == 0:
+                number_is_prime = False
+                break
+
+        if number_is_prime:
+            list.append(current_value_being_checked)
+
+        current_value_being_checked += 1
+
     return list
